a6a49c88e18693f9dba9f5e1284c71119b6855d1
RicoFung <ricofungcn@icloud.com>
2018-07-27 09:10:32 +0800

    添加 Security HttpBasic认证

--- src/main/java/com/webconfig/WebSecurityConfig.java
+++ src/main/java/com/webconfig/WebSecurityConfig.java
@@ -1,25 +1,77 @@
 package com.webconfig;
 
-//import javax.servlet.http.HttpServletResponse;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-//import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//import org.springframework.util.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
-//@Configuration
-//@EnableWebSecurity
-//@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
-//public class WebSecurityConfig extends WebSecurityConfigurerAdapter
-//{
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter
+{
+	////////////////////////////////////////////////////////////
+	// HttpBasic 认证
+	////////////////////////////////////////////////////////////
+	@Autowired
+	private Environment env;
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception
+	{
+		http.authorizeRequests().antMatchers("/**").authenticated().and().httpBasic();
+		http.csrf().disable();
+	}
+
+	@Bean
+	public UserDetailsService userDetailsService()
+	{
+		// Get the user credentials from the console (or any other source):
+		String username = env.getProperty("spring.security.user.name");
+		String password = env.getProperty("spring.security.user.password");
+
+		// Set the inMemoryAuthentication object with the given credentials:
+		InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+		String encodedPassword = passwordEncoder().encode(password);
+		manager.createUser(User.withUsername(username).password(encodedPassword).roles("ADMIN").build());
+		return manager;
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder()
+	{
+		return new BCryptPasswordEncoder();
+	}
+	
+	////////////////////////////////////////////////////////////
+	//	 自定义认证，开发中...
+	////////////////////////////////////////////////////////////
+//	@Autowired
+//	MyUserDetailsService service;
+//	@Autowired
+//    private AuthenticationEntryPoint authenticationEntryPoint;
+//	
 //	@Override
 //	protected void configure(HttpSecurity http) throws Exception
 //	{
 //		// super.configure(http);
 //
+////		 http
+//////		 .csrf().disable() // ajax请求需禁用CSRF
+////		 .authorizeRequests()
+////		 .anyRequest().authenticated()//.antMatchers("/login","/static/**").permitAll()
+////		 .and()
+////		 .formLogin().and()
+////		 .exceptionHandling()
+////         .defaultAuthenticationEntryPointFor(authenticationEntryPoint, new AjaxRequestMatcher())
+////         .and()
+////		 .httpBasic();
+//		
 //		 http
 //		 .csrf().disable() // ajax请求需禁用CSRF
 //		 .authorizeRequests()
@@ -30,7 +82,17 @@ package com.webconfig;
 //
 ////		http.csrf().disable(); // ajax请求需禁用CSRF
 //
-////		http.exceptionHandling().authenticationEntryPoint((request, response, authException) ->
+////		http
+////		.authorizeRequests()
+////		.anyRequest().authenticated()
+////		.antMatchers("/login","/css/**", "/js/**","/fonts/**").permitAll()
+////		.and()
+////		 .formLogin()
+////		 .loginPage("/login").
+////		 and()
+////		 .httpBasic()
+////		 .and()
+////		.exceptionHandling().authenticationEntryPoint((request, response, authException) ->
 ////		{
 ////			String requestType = request.getHeader("x-requested-with");
 ////			if (!StringUtils.isEmpty(requestType))
@@ -41,7 +103,7 @@ package com.webconfig;
 ////			}
 ////			else
 ////			{
-////				response.sendRedirect("/security/login");
+////				response.sendRedirect("/chok-demo/login");
 ////			}
 ////		});
 //	}
@@ -49,7 +111,16 @@ package com.webconfig;
 //	@Override
 //	protected void configure(AuthenticationManagerBuilder auth) throws Exception
 //	{
+////		PasswordEncoder passwordEncoder = PasswordEncoderFactory.createDelegatingPasswordEncoder();
+////		auth.userDetailsService(service).passwordEncoder(passwordEncoder);
+////		auth.userDetailsService(service).passwordEncoder(new Md5PasswordEncoder());
 //		auth.inMemoryAuthentication().withUser("root").password("root").roles("USER");
 //	}
 //
-//}
+//    @Override
+//    public void configure(WebSecurity web) throws Exception 
+//    {
+//        web.ignoring().antMatchers("/static");
+//    }
+	
+}
